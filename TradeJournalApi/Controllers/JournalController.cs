8e533e51e2f88a3e8972a303cc2cc1a59b989de6
A\narlante <narlante@mssngsolutions.ph>
2018-05-22 14:33:55 +0800

    Initial push commit

---
+++ TradeJournalApi/Controllers/JournalController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TradeJournalApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class JournalController : Controller
+    {
+        // GET: api/Journal
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Journal/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Journal
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Journal/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
